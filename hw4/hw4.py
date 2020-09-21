b5cf2ee2de2f9ed654c9ceae2386af2754129524
Yanling Wang <yanling.psu@gmail.com>
2020-09-21 05:31:49 +0000

    Adding hw4.

---
+++ hw4/hw4.py
@@ -0,0 +1,11 @@
+# Author: Yanling Wang yuw17@psu.edu
+def getGradePoint(letter):
+  """use if/elif/elif/.../else structure to implement getGradePoint"""
+  return 0.0
+
+def run():
+  """use for i in range(n): loop to simplify hw2"""
+  return
+
+if __name__ == "__main__":
+  run()
