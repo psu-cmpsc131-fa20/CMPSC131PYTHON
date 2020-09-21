d25b39d58378ba62fe0dab2076835ca13be3d7f6
Yanling Wang <yanling.psu@gmail.com>
2020-09-21 05:17:52 +0000

    Initial commit

---
+++ lab4/main.py
@@ -0,0 +1,24 @@
+# Author: Yanling Wang yuw17@psu.edu
+# Collaborator:
+# Collaborator:
+# Collaborator:
+# Section: 1
+# Breakout: 12
+def num_of_divisors(n):
+  return 0
+
+def sum_n(n):
+  return 0
+
+def print_n(s, n):
+  return
+
+def run():
+  num = int(input("Enter an int: "))
+  print(f"sum is {sum_n(num)}.")
+  line = input("Enter a string: ")
+  print_n(line, num)
+  print(f"{num} has {num_of_divisors(num)} divisors.")
+
+if __name__ == "__main__":
+  run()
