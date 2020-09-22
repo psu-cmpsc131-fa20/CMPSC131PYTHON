ea4df3b3902c05231bb4a83b2da7a16599e39cb5
Yanling Wang <yuw17@psu.edu>
2020-09-22 02:39:59 -0400

    Added requirement for each function.

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -4,13 +4,27 @@
 # Collaborator:
 # Section: 1
 # Breakout: 12
+
 def num_of_divisors(n):
+  """
+  given a positive int n, return number of divisors for n between 1-n inclusive
+  You must use a while cond: style loop for this function.
+  """
+
   return 0
 
 def sum_n(n):
+  """
+  given a non-negative int n, return the sum 0+1+2+...+n
+  You must use a while cond: style loop for this function.
+  """
   return 0
 
 def print_n(s, n):
+  """
+  given a string s and a non-negative int n, print s n times
+  you must use for ... in range(...): style loop for this function
+  """
   return
 
 def run():
