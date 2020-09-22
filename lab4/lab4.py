23d3054ddd3ab04c331da46bf41f0d4114254849
Yanling Wang <yuw17@psu.edu>
2020-09-22 03:18:58 -0400

    Adding num_of_primes function to lab4.

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -10,7 +10,14 @@ def num_of_divisors(n):
   given a positive int n, return number of divisors for n between 1-n inclusive
   You must use a while cond: style loop for this function.
   """
+  return 0
 
+def num_of_primes(n):
+  """
+  given a positive int n, return number of primes that is <= n.
+  Use num_of_divisors() function to help determine if a number is prime:
+  a number is a prime if its number of divisors is 2.
+  """
   return 0
 
 def sum_n(n):
@@ -33,6 +40,7 @@ def run():
   line = input("Enter a string: ")
   print_n(line, num)
   print(f"{num} has {num_of_divisors(num)} divisors.")
+  print(f"There are {num_of_primes(num)} primes <= {num}.")
 
 if __name__ == "__main__":
   run()
