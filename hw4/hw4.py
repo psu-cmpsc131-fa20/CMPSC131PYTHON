26327fd31ef15bcd91d13137e8f3522f0d3e03bb
Yanling Wang <yuw17@psu.edu>
2020-09-23 14:33:18 -0400

    hw4 comments

---
+++ hw4/hw4.py
@@ -0,0 +1,21 @@
+# Author: Yanling Wang yuw17@psu.edu
+def getGradePoint(letter):
+  """
+  Given a letter grade, translate it into a gradepoint value between 0.0 and 4.0
+  YOU MUST USE if/elif/../else structure to implement this function.
+  YOU CAN NOT use any features/data structure we haven't learned in the class,
+  including but not limited to list, dictionary and tuples.
+  """
+  return 0.0
+
+def run():
+  """
+  Use either a while-loop or a for-loop to make your code for getting three separate
+  courses' letter grade and credit info. And calculate the final GPA.
+  YOU CAN NOT use any features/data structures we haven't learned in the class,
+  including but not limited to list, dictionary, and tuples.
+  """
+  return
+
+if __name__ == "__main__":
+  run()
