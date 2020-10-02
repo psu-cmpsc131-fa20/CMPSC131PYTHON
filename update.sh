4741ebdd0e1b7a55b2f28261a3ff62f45da9b18a
Asmit De <asmit.de@outlook.com>
2020-10-02 13:40:55 -0400

    Bugfix pre-commit in update

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-git commit -a "Any uncommitted changes."
+git add .
+git commit -m "Any uncommitted changes."
 git pull staff master --allow-unrelated-histories --no-edit
 git push origin master
