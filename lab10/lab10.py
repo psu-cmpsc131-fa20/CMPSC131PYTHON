ca9c401584d596c3dfdb5715879603a23d33a2b6
Yanling Wang <yuw17@psu.edu>
2020-11-03 13:11:12 +0000

    lab10 starter code

---
+++ lab10/lab10.py
@@ -0,0 +1,38 @@
+# Author: Yanling Wang yuw17@psu.edu
+# Collaborator:
+# Collaborator:
+# Collaborator:
+# Section: 1
+# Breakout: 1
+
+def remove_duplicate_sorted(t):
+  """
+  this function returns a new list generated from t that has t's
+  elements without duplicates and is sorted from smallest to largest.
+  """
+  return [] 
+
+def list_to_dictionary(t):
+  """
+  t is a list of values (values could be str, list, tuple, set, dictionary),
+  create and return a dictionary such that the key is len(v) for some v in t,
+  and the value is a list of values [v1, v2, ...] from t (in the order they
+  appeared in t) whose len(vi) is the key.
+  for example: if t = [(1,2,3), "abc", [1, 2], (), ""]
+  then return value of this function should be:
+  {3: [(1, 2, 3), 'abc'], 2: [[1, 2]], 0: [(), '']}
+  """
+  return {} 
+
+def run():
+  """
+  This function repeatedly ask user to enter a string and store them in a
+  list and print it out.
+  It also passes this list to remove_duplicate_sorted() function and
+  list_to_dictionary() function and print out the results of the function
+  calls.
+  """
+  return
+
+if __name__ == "__main__":
+  run()
