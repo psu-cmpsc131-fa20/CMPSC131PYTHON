22473e9d1c30a1d18a353f9634d67cc855ec4dd4
Yanling Wang <yuw17@psu.edu>
2020-11-15 09:47:01 +0000

    project1 starter code and data

---
+++ project1/dna.py
@@ -0,0 +1,12 @@
+from sys import argv
+import csv
+import time
+
+def run():
+  return
+
+if __name__ == "__main__":
+  start = time.perf_counter()
+  run()
+  end = time.perf_counter()
+  print(f"Time used: {end-start} seconds")
