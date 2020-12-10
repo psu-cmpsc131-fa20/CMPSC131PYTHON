f9051954353bdf7dafc8727fe3d5a4692c9a9096
Yanling Wang <yuw17@psu.edu>
2020-12-10 00:39:55 +0000

    quiz3 starter code and supporting files

---
+++ quiz3/quiz3a.py
@@ -0,0 +1,15 @@
+# Author: Yanling Wang yuw17@psu.edu
+# GitHub ID: yanlingpsu
+
+"""
+Your program should print out two lines, each line containing a number
+that is the answer for the quiz question 2 on Canvas.
+
+You can assume the words.txt file is in the same directory as your quiz3a.py
+Your program has to show your work of how you get the numerical answers
+on Canvas Quiz.
+
+Autograder will run your program as follows and expect 2 numbers in the output:
+
+$python3 quiz3a.py
+"""
---
+++ quiz3/sudoku.py
@@ -0,0 +1,10 @@
+# Author: Yanling Wang yuw17@psu.edu
+# GitHub ID: yanlingpsu
+
+"""
+Your program should read in a pickle file that encodes a sudoku puzzle,
+print out a puzzle, and analyze possible answers for a given coordinate
+(x,y).
+$python3 sudoku.py sudokus/s01a.p1 3 5 answer.csv
+$python3 sudoku.py sudokus/s01a.p1 0 1 answer.csv
+"""
